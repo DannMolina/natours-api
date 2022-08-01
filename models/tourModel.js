66fdc3453d0285310dd8d883dffda085a43cf1a0
Dann Russell Molina <molinadannrussell.dev@gmail.com>
2022-08-01 23:09:50 +0800

    Virtual Properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -3,64 +3,81 @@ const mongoose = require('mongoose'); // mongoDB driver
 /**
  * SCHEMA
  */
-const tourSchema = new mongoose.Schema({
-    /**
-     * property:{}
-     * sample of schema type options and they can be different for different types
-     * and can add validation options
-     * see: documents for more options
-     */
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true,
-        trim: true,
+const tourSchema = new mongoose.Schema(
+    {
+        /**
+         * property:{}
+         * sample of schema type options and they can be different for different types
+         * and can add validation options
+         * see: documents for more options
+         */
+        name: {
+            type: String,
+            required: [true, 'A tour must have a name'],
+            unique: true,
+            trim: true,
+        },
+        duration: {
+            type: Number,
+            required: [true, 'A tour must have a duration'],
+        },
+        maxGroupSize: {
+            type: Number,
+            required: [true, 'A tour must have a group size'],
+        },
+        difficulty: {
+            type: String,
+            required: [true, 'A tour must have a difficulty'],
+        },
+        ratingsAverage: {
+            type: Number,
+            default: 4.5,
+        },
+        ratingsQuantity: {
+            type: Number,
+            default: 0,
+        },
+        price: {
+            type: Number,
+            required: [true, 'A tour must have a price'],
+        },
+        priceDiscount: Number,
+        summary: {
+            type: String,
+            trim: true, // * it only works on string, will remove all the white spaces in the beginning and end of the string
+            required: [true, 'A tour must have a description'],
+        },
+        description: {
+            type: String,
+            trim: true,
+        },
+        imageCover: {
+            type: String,
+            required: [true, 'A tour must have a cover image'],
+        },
+        images: [String],
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            select: false, // exclude this field, option to not include in the response object, default to true
+        },
+        startDates: [Date],
     },
-    duration: {
-        type: Number,
-        required: [true, 'A tour must have a duration'],
+    {
+        // oject for the options
+        toJSON: { virtuals: true }, // virtuals be part of the output
+        toObject: { virtuals: true },
     },
-    maxGroupSize: {
-        type: Number,
-        required: [true, 'A tour must have a group size'],
-    },
-    difficulty: {
-        type: String,
-        required: [true, 'A tour must have a difficulty'],
-    },
-    ratingsAverage: {
-        type: Number,
-        default: 4.5,
-    },
-    ratingsQuantity: {
-        type: Number,
-        default: 0,
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price'],
-    },
-    priceDiscount: Number,
-    summary: {
-        type: String,
-        trim: true, // * it only works on string, will remove all the white spaces in the beginning and end of the string
-        required: [true, 'A tour must have a description'],
-    },
-    description: {
-        type: String,
-        trim: true,
-    },
-    imageCover: {
-        type: String,
-        required: [true, 'A tour must have a cover image'],
-    },
-    images: [String],
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-        select: false, // exclude this field, option to not include in the response object, default to true
-    },
-    startDates: [Date],
+);
+
+/**
+ * Business logic: compute the duration in weeks
+ * this virtual property here will basically be created each time that we get some data out of the database.
+ * use regular function instead of arrow function because arrow function does not get the 'this' keyword
+ * 'durationWeeks' property is not part of the database
+ */
+tourSchema.virtual('durationWeeks').get(function () {
+    return this.duration / 7; // "this" keyword is currently pointing to the current document
 });
 
 /**
